d0bac5705fe68aad0e911d500f1b78e2b2351112
Malone <malone@gmail.com>
2025-03-19 07:41:43 +0800

    在TokenUtils.java中设置静态方法getCurrentUser用于获取当前用户信息

--- springboot/src/main/java/com/example/utils/TokenUtils.java
+++ springboot/src/main/java/com/example/utils/TokenUtils.java
@@ -2,12 +2,39 @@ package com.example.utils;
 
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.example.entity.Account;
+import com.example.service.AdminService;
+import com.example.service.UserService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Date;
 
+@Component
 public class TokenUtils {
+
+    @Resource
+    AdminService adminService;
+    @Resource
+    UserService userService;
+
+    static AdminService staticAdminService;
+    static UserService staticUserService;
+
+    // Springboot 工程启动后会加载这段代码
+    @PostConstruct
+    public void init() {
+        staticAdminService = adminService;
+        staticUserService = userService;
+    }
+
     /**
      * 生成Token
      */
@@ -15,5 +42,29 @@ public class TokenUtils {
         return JWT.create().withAudience(data) // 将userId-role 保存到 token 里面作为载荷
                 .withExpiresAt(DateUtil.offsetDay(new Date(), 1)) // 1 天后 token 过期
                 .sign(Algorithm.HMAC256(sign)); // 以 password 作为 token 的密钥，
+    }
+
+    /**
+     * 获取当前登陆的用户信息
+     */
+    public static Account getCurrentUser() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String token = request.getHeader("token");
+        if (StrUtil.isBlank(token)) {
+            token = request.getParameter("token");
+        }
+
+        // 拿到Token的载荷数据
+        String audience = JWT.decode(token).getAudience().get(0);
+        String[] split = audience.split("-");
+        String userId = split[0];
+        String role = split[1];
+        // 根据token解析出来的userId去对应的表查询用户信息
+        if ("ADMIN".equals(role)) {
+            return staticAdminService.selectById(userId);
+        } else if ("USER".equals(role)) {
+            return staticUserService.selectById(userId);
+        }
+        return null;
     }
 }
