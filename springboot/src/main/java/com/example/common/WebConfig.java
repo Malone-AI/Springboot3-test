634012b9216d8a9dc740c6a38eac63b007eae0b6
Malone <malone@gmail.com>
2025-03-19 06:59:15 +0800

    WebConfig.java => Spring MVC 的配置类
    - 向Spring注册JWT拦截器
    - 配置拦截器的作用范围
    - 通过@Bean注解创建JWTInterceptor实例，使其被Spring容器管理
    - WebConfig.java 告诉系统哪些请求需要验证
    - JWTInterceptor.java 负责‘如何验证’这些请求

--- springboot/src/main/java/com/example/common/WebConfig.java
+++ springboot/src/main/java/com/example/common/WebConfig.java
@@ -1,4 +1,22 @@
 package com.example.common;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor())
+                .addPathPatterns("/**") // 验证所有页面
+                .excludePathPatterns("/login", "/register"); // 除去/login 和 /register 页面都进行验证
+    }
+
+    @Bean
+    public JWTInterceptor jwtInterceptor() {
+        return new JWTInterceptor();
+    }
 }
