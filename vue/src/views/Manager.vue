3c7a72dad0ca706f1f75ca7e85cbda7bbc4f5023
Malone <malone@gmail.com>
2025-03-19 20:04:34 +0800

    修改前端Manager.vue使左上角显示头像
    - 如果用户有头像就显示用户上传的头像
    - 否则显示默认头像

--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -13,8 +13,9 @@
       <div style="width: fit-content; display: flex; align-items: center; padding-right: 20px; border-bottom: 1px solid #ddd">
         <el-dropdown>
           <div style="display: flex; align-items: center;">
-            <img style="width: 40px; height: 40px; border-radius: 50%" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" alt="">
-            <span style="margin-left: 5px">{{ userInfo.name || userInfo.username}}</span>
+            <img v-if="data.user?.avatar" style="width: 40px; height: 40px; border-radius: 50%" :src="data.user?.avatar" alt="">
+            <img v-else style="width: 40px; height: 40px; border-radius: 50%" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" alt="">
+            <span style="margin-left: 5px">{{ data.name || data.username}}</span>
           </div>
           <template #dropdown>
             <el-dropdown-menu>
@@ -64,7 +65,11 @@
 <script setup>
 
 import router from "@/router/index.js";
-import {ref} from 'vue'
+import {reactive, ref} from 'vue'
+
+const data = reactive({
+  user: JSON.parse(localStorage.getItem('code_user'))
+})
 
 const userInfo = ref({})
 let userStr = localStorage.getItem('code_user');
