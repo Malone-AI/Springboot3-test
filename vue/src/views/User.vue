10333d6c021bd1a2273733aaae06e4909e3650b8
Malone <malone@gmail.com>
2025-03-19 18:16:44 +0800

    给User用户权限，添加头像上传功能
    - 在UserService中创建token并返回给前端
    - 前端User.vue的批量导出请求中拼接token
    - 前端User.vue中添加头像上传按钮

--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -59,6 +59,16 @@
         <el-form-item prop="email" label="邮箱">
           <el-input v-model="data.form.email" autocomplete="off" />
         </el-form-item>
+        <el-form-item prop="avatar" label="头像">
+          <el-upload
+            action="http://localhost:9999/files/upload"
+            :headers="{token: data.user.token}"
+            :on-success="handlerFileSuccess"
+            list-type="picture"
+          >
+            <el-button type="primary">上传头像</el-button>
+          </el-upload>
+        </el-form-item>
       </el-form>
       <template #footer>
         <div class="dialog-footer">
@@ -77,6 +87,7 @@ import {Search} from "@element-plus/icons-vue";
 import request from "@/utils/request.js";
 import {ElMessage, ElMessageBox} from "element-plus";
 const data = reactive({
+  user: JSON.parse(localStorage.getItem('code_user') || '{}'),
   username: null,
   name: null,
   pageNum: 1,
@@ -215,10 +226,11 @@ const deleteBatch = () => {
 const exportData = () => {
   if (data.rows.length > 0) {
     const ids = data.rows.map(item => item.id)
-    let url = `http://localhost:9999/user/exportBatch?ids=${ids}`
+    let url = `http://localhost:9999/admin/exportBatch?ids=${ids}&token=${data.user.token}`
     window.open(url)
   } else {
-    let url = `http://localhost:9999/user/export?username=${data.username === null ? '' : data.username}&name=${data.name === null ? '' : data.name}`
+    let url = `http://localhost:9999/admin/export?username=${data.username === null ? '' : data.username}&name=${data.name === null ? '' : data.name}`
+        + `&token=${data.user.token}`
     window.open(url)
   }
 }
@@ -226,6 +238,10 @@ const exportData = () => {
 const handlerImport = () => {
   ElMessage.success("批量导入成功")
   load()
+}
+
+const handlerFileSuccess = (res) => {
+  data.form.avatar = res.data
 }
 
 </script>
